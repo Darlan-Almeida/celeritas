ea84680f61e98ef84affa9131f58422d49f975ae
Darlan Almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-09-05 17:43:12 +0000

    adicionando dados de treino

---
+++ index.py
@@ -0,0 +1,13 @@
+from sklearn import tree
+import pandas as pd
+
+
+data =pd.read_csv('treino.csv')
+"""
+treino = pd.DataFrame(data.data)
+treino.columns=  data.feature_names
+treino['target'] = treino.target"""
+data.head(3)
+
+
+
